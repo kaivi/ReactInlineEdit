f8bf9ddc7cd6f5de9ef73b420415f24068a1ae88
Kai Vik <kvi@ieee.org>
2015-06-15 01:08:11 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
-# EditableInlineText
-Simple ES6 React component for in-place text editing
+# Inline Edit Component for React
+Simple React component for in-place text editing. It turns into an `<input />` when disturbed, and tries to validate and save input on <kbd>Enter</kbd> or `blur`. <kbd>Esc</kbd> works as well for cancelling. Use it with [Babel](https://babeljs.io/) because of ECMAScript 6 and JSX Harmony.
+
+![Example animation gif](http://i.imgur.com/8vig5m1.gif)
+
+### Installation
+
+`npm install react-inline-edit --save-dev`
+
+### Required props
+- `text`:`string` initial text
+- `paramName`:`string` name of the parameter to be returned to `change` function
+- `change`:`function` function to call when new text is changed and validated, it will receive `{paramName: value}`
+
+### Optional props
+- `activeClassName`:`string` class to apply when in edit mode
+- `validate`:`function` boolean function for custom validation, using this overrides the two props below
+- `minLength`:`number` minimum text length, **default** `1`
+- `maxLength`:`number` maximum text length, **default** `256`
+
+### Usage example
+```javascript
+import React from 'react';
+import InlineEdit from 'react-inline-edit';
+
+class MyParentComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.dataChanged = this.dataChanged.bind(this);
+    }
+
+    dataChanged(data) {
+        // data = { description: "New validated text comes here" }
+        // Update your model from here
+    }
+
+    customValidateText(text) {
+        return (text.length > 8 && text.length < 64);
+    }
+
+    render() {
+        return (<div>
+            <h2>Edit this string</h2>
+            <InlineEdit
+              validate={this.customValidateText}
+              activeClassName="editing"
+              text={this.props.myObject.description}
+              paramName="description"
+              change={this.dataChanged}
+            />
+        </div>)
+    }
+}
+
+export default MyParentComponent;
+
+```
---
+++ index.js
@@ -0,0 +1,96 @@
+import React from 'react';
+
+function SelectInputText(element) {
+    element.setSelectionRange(0, element.value.length);
+}
+
+class InlineEdit extends React.Component {
+    constructor(props) {
+        super(props);
+        this.startEditing = this.startEditing.bind(this);
+        this.finishEditing = this.finishEditing.bind(this);
+        this.textChanged = this.textChanged.bind(this);
+        this.isInputValid = this.isInputValid.bind(this);
+        this.componentDidUpdate = this.componentDidUpdate.bind(this);
+        this.commitEditing = this.commitEditing.bind(this);
+        this.keyDown = this.keyDown.bind(this);
+        this.state = {
+            editing: false,
+            text: this.props.text,
+            minLength: this.props.minLength || 1,
+            maxLength: this.props.maxLength || 256
+        };
+        this.isInputValid = this.props.validate || this.isInputValid.bind(this);
+    }
+
+    startEditing() {
+        this.setState({editing: true, text: this.props.text});
+    }
+
+    finishEditing() {
+        if(this.isInputValid(this.state.text) && this.props.text != this.state.text){
+            this.commitEditing();
+        } else if (this.props.text === this.state.text || !this.isInputValid(this.state.text)) {
+            this.cancelEditing();
+        }
+    }
+
+    cancelEditing() {
+        this.setState({editing: false, text: this.props.text});
+    }
+
+    commitEditing() {
+        this.setState({editing: false, text: this.state.text});
+        let newProp = {};
+        newProp[this.props.paramName] = this.state.text;
+        this.props.change(newProp);
+    }
+
+    isInputValid(text) {
+        return (text.length >= this.state.minLength && text.length <= this.state.maxLength);
+    }
+
+    keyDown(event) {
+        if(event.keyCode === 13) {
+            this.finishEditing();
+        } else if (event.keyCode === 27) {
+            this.cancelEditing();
+        }
+    }
+
+    textChanged(event) {
+        this.setState({
+            text: event.target.value.trim()
+        })
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        var inputElem = React.findDOMNode(this.refs.input);
+        if (this.state.editing && !prevState.editing) {
+            inputElem.focus();
+            SelectInputText(inputElem);
+        } else if (this.state.editing && prevProps.text != this.props.text) {
+            this.finishEditing();
+        }
+    }
+
+    render() {
+        if(!this.state.editing) {
+            return <span onClick={this.startEditing}>{this.props.text}</span>
+        } else {
+            return <input className={this.props.activeClassName} onKeyDown={this.keyDown} onBlur={this.finishEditing} ref="input" defaultValue={this.state.text} onChange={this.textChanged} onReturn={this.finishEditing} />
+        }
+    }
+}
+
+InlineEdit.propTypes = {
+    text: React.PropTypes.string.isRequired,
+    paramName: React.PropTypes.string.isRequired,
+    change: React.PropTypes.func.isRequired,
+    activeClassName: React.PropTypes.string,
+    minLength: React.PropTypes.number,
+    maxLength: React.PropTypes.number,
+    validate: React.PropTypes.func
+};
+
+export default InlineEdit;
